72921e7a792ebe17e1b9ba085895c666cd017ce4
José Adriano Emmanuel <joseadriano066@gmail.com>
2024-11-28 13:51:33 +0100

    Datacontext

---
+++ src/infrastruture/School.Data/SchoolDBContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace School.Data;
+
+public class SchoolDBContext:DbContext
+{
+    public SchoolDBContext(DbContextOptions<SchoolDBContext> options) : base(options)
+    {
+        
+    }
+}
